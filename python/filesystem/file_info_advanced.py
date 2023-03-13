5eca2dd6746e4f18673b094406ab12a2ebfef6bd
Adeolu Oyinlola <88986951+deoluoyinlola@users.noreply.github.com>
2023-03-13 12:39:54 +0100

    Create file_info_advanced.py

---
+++ python/filesystem/file_info_advanced.py
@@ -0,0 +1,15 @@
+import os
+
+dict_list = []
+
+def extract_info(path = '.'): #function to extract file info, defaulted to present working directory if path is not passed
+    dict_list = []
+
+    for root, dirs, files in os.walk(path): #recursively iterate over all files/directiories in path directory
+        for file_name in files:
+            file_dict = {}
+            file_dict['path'] = os.path.join(root, file_name) #get path name of file
+            file_dict['size'] = os.path.getsize(os.path.join(root, file_name)) #get file size
+            dict_list.append(file_dict) #appends file info dictionary to the list
+    
+    return dict_list #returns list
