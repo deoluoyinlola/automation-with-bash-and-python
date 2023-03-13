cd82b399c4836b30ef282a0eb9eee25ac8b42f40
Adeolu Oyinlola <88986951+deoluoyinlola@users.noreply.github.com>
2023-03-13 12:49:12 +0100

    Update adduser.sh

--- bash/accounts/adduser.sh
+++ bash/accounts/adduser.sh
@@ -17,3 +17,38 @@ fi
 #Showing User ID Group Id and Group 
 echo "Here is additional information for $username"       
  id $username
+ 
+ 
+ 
+ 
+ #
+ #
+ #
+ #
+ 
+ #!/bin/bash
+
+#This script to accept user name(s) as arguments to add new users to server. If one of the user names already exist, it will exit the script, but if it doesnt, it will succefully create the user.
+
+#For loop to iterate through arguments
+for i in $*
+do
+
+	#Check if username is in passwd database and send all other unimportant information to /dev/null
+	getent passwd $i  > /dev/null
+
+	#Use $? to find return value of last command. If username was found, it will return 0. We can check if $username exist by the condition if statement. 
+	 
+		if [ $? -ne 0 ]; 
+
+			#Add user if return value is not equal to 0
+  			then
+				sudo useradd -m $i;
+				echo "$i user has been added!" 
+
+			#Notify that user already exist and exit script
+  			else
+	 			echo "Unsuccessful. $i already exists."
+
+		fi
+done
