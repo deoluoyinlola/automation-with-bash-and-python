3330b486167eafa6f53955672e08549aab012694
Adeolu Oyinlola <88986951+deoluoyinlola@users.noreply.github.com>
2023-04-11 12:53:17 +0100

    Create find_nth_AP_number.py

---
+++ python/misc/find_nth_AP_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nthTermOfAP(self,A1,A2,N):
+        return (A1+((N-1)*(A2-A1)))
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        A1,A2,N=map(int,input().strip().split(' '))
+        s=Solution()
+        print(s.nthTermOfAP(A1,A2,N))
