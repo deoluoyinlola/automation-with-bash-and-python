0fff93f2ba21acecb0e1647d34779f1a6d2e448a
Adeolu Oyinlola <88986951+deoluoyinlola@users.noreply.github.com>
2023-04-11 12:52:07 +0100

    Create find_closest_number.py

---
+++ python/misc/find_closest_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def ClosestNumber(self, N , M):
+        return round(N/float(M))*M
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        N,M=map(int,input().strip().split(" "))
+        s=Solution()
+        print(s.ClosestNumber(N,M))
